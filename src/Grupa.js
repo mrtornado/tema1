e09453dde49b3695d54b50f3bdcd7b1afdd47fdf
Toro <toro@Torel.local>
2019-03-10 10:30:13 +0200

    first upload

---
+++ src/Grupa.js
@@ -0,0 +1,262 @@
+import React, { Component } from "react";
+// import Table from "./components/Table";
+import AdaugaStudent from "./components/AdaugaStudent";
+
+var ID = function() {
+  return (
+    "_" +
+    Math.random()
+      .toString(36)
+      .substr(2, 9)
+  );
+};
+
+class Grupa extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      studenti: [],
+      sort: {
+        column: null,
+        direction: "desc"
+      },
+      campuri: [
+        {
+          numeColoana: "nume",
+          numeDeAfisare: "First Name",
+          afisare: true
+        },
+        {
+          numeColoana: "prenume",
+          numeDeAfisare: "Last Name",
+          afisare: true
+        },
+        {
+          numeColoana: "varsta",
+          numeDeAfisare: "Age",
+          afisare: true
+        },
+        {
+          numeColoana: "media",
+          numeDeAfisare: "Grade",
+          afisare: true
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://demo3305866.mockable.io/tema_studenti_react")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            studenti: result.students.map((x) => {
+              x.key = ID();
+              return x;
+            })
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  onSort = (column) => {
+    return (e) => {
+      const direction = this.state.sort.column
+        ? this.state.sort.direction === "asc"
+          ? "desc"
+          : "asc"
+        : "desc";
+      const sortedStudenti = this.state.studenti.sort((a, b) => {
+        if (column === "nume") {
+          const nameA = a.nume.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.nume.toUpperCase(); // ignore upper and lowercase
+
+          if (nameA < nameB) return -1;
+          if (nameA < nameB) return 1;
+          else return 0;
+        } else if (column === "prenume") {
+          const nameA = a.prenume.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.prenume.toUpperCase(); // ignore upper and lowercase
+
+          if (nameA < nameB) return -1;
+          if (nameA < nameB) return 1;
+          else return 0;
+        } else if (column === "varsta") {
+          const nameA = a.varsta;
+          const nameB = b.varsta;
+
+          if (nameA < nameB) return -1;
+          if (nameA < nameB) return 1;
+          else return 0;
+        } else if (column === "media") {
+          const nameA = a.media;
+          const nameB = b.media;
+
+          if (nameA < nameB) return -1;
+          if (nameA < nameB) return 1;
+          else return 0;
+        } else {
+          return a.nume - b.nume;
+        }
+      });
+
+      if (direction === "desc") {
+        sortedStudenti.reverse();
+      }
+
+      this.setState({
+        studenti: sortedStudenti,
+        sort: {
+          column,
+          direction
+        }
+      });
+    };
+  };
+
+  handleRemove(student) {
+    const studenti = this.state.studenti.filter((s) => s.key !== student.key);
+    this.setState((prevState) => {
+      return {
+        studenti: studenti
+      };
+    });
+  }
+
+  handleAdaugaStudent = (nume, prenume, varsta, media, key) => {
+    const newState = this.state.studenti.concat({
+      nume: nume,
+      prenume: prenume,
+      varsta: varsta,
+      media: media,
+      key: key
+    });
+    this.setState((prevState) => {
+      return {
+        studenti: newState
+      };
+    });
+  };
+
+  render() {
+    const { error, isLoaded, studenti, campuri } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return (
+        <div className="jumbotron">
+          <h1 style={{ textAlign: "center" }}>Loading...</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <h1>
+            Today <span style={{ color: "red" }}> {studenti.length}</span>{" "}
+            students are attending our class!
+          </h1>
+          <table
+            className="table"
+            style={{ border: "2px solid black", borderCollapse: "collapse" }}
+          >
+            <thead>
+              <tr
+                style={{
+                  lineHeight: "25px",
+                  minHeight: "25px",
+                  height: "25px"
+                }}
+              >
+                {campuri.map((x) => {
+                  return x.afisare ? (
+                    <th
+                      style={{
+                        textAlign: "center",
+                        color: "black",
+                        width: "25%",
+                        border: "2px solid black",
+                        backgroundColor: "#FFF8DD",
+                        padding: "10px"
+                      }}
+                      id={x.numeColoana}
+                      key={ID()}
+                      onClick={this.onSort(x.numeColoana)}
+                    >
+                      {x.numeDeAfisare}
+                    </th>
+                  ) : null;
+                })}
+                <th
+                  style={{
+                    border: "2px solid black",
+                    backgroundColor: "#FFF8DD"
+                  }}
+                />
+              </tr>
+            </thead>
+            <tbody>
+              {studenti.map((student) => {
+                return (
+                  <tr
+                    style={{
+                      lineHeight: "25px",
+                      minHeight: "25px",
+                      height: "25px"
+                    }}
+                    key={ID()}
+                  >
+                    {campuri.map((y) => {
+                      return y.afisare ? (
+                        <td
+                          key={ID()}
+                          style={{
+                            border: "2px solid black",
+                            backgroundColor: "#FFF8DD",
+                            textAlign: "center",
+                            color: "red"
+                          }}
+                        >
+                          {student[y.numeColoana]}
+                        </td>
+                      ) : null;
+                    })}
+                    <td
+                      style={{
+                        backgroundColor: "#FFF8DD",
+                        border: "1px solid black"
+                      }}
+                    >
+                      <button
+                        className="btn btn-success btn-sm active"
+                        onClick={() => this.handleRemove(student)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <AdaugaStudent
+            handleAdaugaStudent={this.handleAdaugaStudent}
+            studCount={studenti.length}
+          />
+        </div>
+      );
+    }
+  }
+}
+
+export default Grupa;
---
+++ src/components/AdaugaStudent.js
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+
+var ID = function() {
+  return (
+    "_" +
+    Math.random()
+      .toString(36)
+      .substr(2, 9)
+  );
+};
+
+class AdaugaStudent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nume: "",
+      prenume: "",
+      varsta: "",
+      media: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.form = React.createRef();
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const key = ID();
+    const { nume, prenume, varsta, media } = this.state;
+    const validat = this.validate();
+
+    if (validat && this.props.studCount < 9) {
+      this.props.handleAdaugaStudent(nume, prenume, varsta, media, key);
+      this.setState({
+        nume: "",
+        prenume: "",
+        varsta: "",
+        media: "",
+        key: ""
+      });
+    }
+    console.log("No more then 9 students allowed");
+  }
+
+  validate() {
+    return this.form.current.reportValidity();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form
+          ref={this.form}
+          name="adauga"
+          onSubmit={this.handleSubmit}
+          style={{ marginTop: "10px" }}
+        >
+          <input
+            className="form-control"
+            aria-label="Small"
+            aria-describedby="inputGroup-sizing-sm"
+            name="nume"
+            value={this.state.nume}
+            onChange={this.handleChange}
+            placeholder="First Name"
+            required
+            minLength="4"
+            type="text"
+          />
+          <br />
+          <input
+            className="form-control"
+            aria-label="Small"
+            aria-describedby="inputGroup-sizing-sm"
+            name="prenume"
+            value={this.state.prenume}
+            onChange={this.handleChange}
+            placeholder="Last Name"
+            required
+            minLength="4"
+            type="text"
+          />
+          <br />
+          <input
+            className="form-control"
+            aria-label="Small"
+            aria-describedby="inputGroup-sizing-sm"
+            name="varsta"
+            value={this.state.varsta}
+            onChange={this.handleChange}
+            placeholder="Age"
+            required
+            pattern="\d+"
+            title="Please use only positive numbers"
+            maxLength="4"
+            type="text"
+          />
+          <br />
+          <input
+            className="form-control"
+            aria-label="Small"
+            aria-describedby="inputGroup-sizing-sm"
+            name="media"
+            value={this.state.media}
+            onChange={this.handleChange}
+            placeholder="Grade"
+            required
+            pattern="\d{0,2}(\.\d{0,1})"
+            title="Please use a decimal number (example: 8.7)"
+            type="text"
+          />
+          <br />
+          <button
+            style={{ marginTop: "10px" }}
+            className="btn btn-raised btn-success"
+            onClick={this.handleSubmit}
+          >
+            Add Student
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AdaugaStudent;
